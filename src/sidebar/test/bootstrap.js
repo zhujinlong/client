3cd0e090bf8fc51ce600e78636be3f41c08b7e85
Robert Knight <robertknight@gmail.com>
2020-07-13 12:31:59 +0100

    Add script loading debugging logic to test startup logic

--- src/sidebar/test/bootstrap.js
+++ src/sidebar/test/bootstrap.js
@@ -23,3 +23,47 @@ registerIcons({
   ...sidebarIcons,
   ...annotatorIcons,
 });
+
+const addedScripts = new Set();
+
+const getFilename = src => {
+  const m = src.match(/[^/]*\.(js|coffee)/);
+  return m ? m[0] : src;
+};
+
+const mo = new MutationObserver(mutationList => {
+  for (let entry of mutationList) {
+    if (entry.type === 'childList') {
+      entry.addedNodes.forEach(r => {
+        if (r.localName === 'script') {
+          const src = r.src;
+          const filename = getFilename(src);
+          if (src) {
+            addedScripts.add(src);
+            console.log(
+              'Added script with src',
+              filename,
+              'count',
+              addedScripts.size
+            );
+            r.addEventListener('load', () => {
+              console.log('Script loaded', filename);
+            });
+            r.addEventListener('error', () => {
+              console.log('Script errored', filename);
+            });
+          } else {
+            console.log('Added anonymous script', r.textContent);
+          }
+        }
+      });
+
+      entry.removedNodes.forEach(r => {
+        if (addedScripts.has(r)) {
+          console.log('Removed script with src ', getFilename(r.src));
+        }
+      });
+    }
+  }
+});
+mo.observe(document.body, { childList: true });
